9e2a8781eb1ecb6e86f439c0899090a3626d5507
MACBOOK <beratkaya1998@gmail.com>
2020-09-09 13:27:30 +0300

    Updates

---
+++ precache-manifest.ee62078ee592a0ff8438dab94f2f9dfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57f51908a15f0bf6695c3ce2dff8648",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8d23351a1a062c593c5f",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "68391eca917be8995fcf",
+    "url": "./static/js/2.f4096692.chunk.js"
+  },
+  {
+    "revision": "8d23351a1a062c593c5f",
+    "url": "./static/js/main.d2be88d7.chunk.js"
+  },
+  {
+    "revision": "fe6f47fb759a4e48adcf",
+    "url": "./static/js/runtime-main.e4622af3.js"
+  }
+]);
